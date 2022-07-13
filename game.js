4be67d18c0a7879d940cc24f0cd19d8d5fc7cd9c
ardhi <ardhi333@gmail.com>
2022-07-13 15:45:48 +0700

    list nama pada highscore blm muncul dan file blm rapih

---
+++ game.js
@@ -0,0 +1,108 @@
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll(".choice-text"));
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+const progressBarFull = document.querySelector("#progressBarFull");
+
+let currentQuestion = {};
+
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+  {
+    question: "What is 2 + 2 ?",
+    choice1: "1",
+    choice2: "4",
+    choice3: "21",
+    choice4: "17",
+    answer: 2,
+  },
+  {
+    question: "What is your firstName ?",
+    choice1: "Ardhi",
+    choice2: "Zenal",
+    choice3: "Diki",
+    choice4: "Nella",
+    answer: 1,
+  },
+  {
+    question: "What is the village that you live in ?",
+    choice1: "Ciletuh",
+    choice2: "Megamendung",
+    choice3: "Cikarang",
+    choice4: "Wetan",
+    answer: 1,
+  },
+  {
+    question: "What is 2 + 5 ?",
+    choice1: "7",
+    choice2: "4",
+    choice3: "21",
+    choice4: "17",
+    answer: 1,
+  },
+];
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestions();
+};
+
+getNewQuestions = () => {
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("/end.html");
+  }
+
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+
+  currentQuestion = availableQuestions[questionsIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((c) => {
+    const number = c.dataset["number"];
+    c.innerText = currentQuestion["choice" + number];
+  });
+  availableQuestions.splice(questionsIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach((c) => {
+  c.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    if (classToApply === "correct") {
+      incrementScore(SCORE_POINTS);
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestions();
+    }, 1000);
+  });
+});
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
